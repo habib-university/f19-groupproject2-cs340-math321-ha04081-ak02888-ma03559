8c4ae21b80b117c0f2b090a966ef9e2b466c3821
Aiman Khan <aiman97khan@gmail.com>
2019-10-11 10:57:29 +0500

    q1.1 done

---
+++ code/bezierBasisFunctions.py
@@ -0,0 +1,52 @@
+# Import our modules that we are using
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from scipy import special as sp
+from sympy import *
+
+def getBezierCurveBasisFunctions(n):
+    t = symbols('t')
+    return [str(int(sp.comb(n,i))*((1-t)**(n-i))*(t**i)) for i in range(n+1) ]
+
+
+def getBezierSurfaceBasisFunctions(n):
+    s = symbols('s')
+    t = symbols('t')
+    return [str(int(sp.comb(n,i))*((1-s)**(n-i))*(s**i)*int(sp.comb(n,j))*((1-t)**(n-j))*(t**j)) for i in range(n+1) for j in range(n+1) ]
+    
+def plotBezierBasisFunctions(type='curve',degree=1):
+    t = np.arange(0,1+0.01,0.01)
+    if (type == 'curve'):
+        basisFunctions = getBezierCurveBasisFunctions(degree)
+        for func in basisFunctions:
+            plt.plot(t,eval(func, {'t':t}),label = func)
+        plt.title("Bezier curves for degree " + str(degree))
+        plt.xlabel('t')
+        plt.ylabel('y axis')
+        plt.grid(alpha=.4,linestyle='--')
+        plt.legend()
+        plt.show()
+    if (type == 'surface'):
+        s = np.arange(0,1+0.01,0.01)
+        T, S = np.meshgrid(t, s)
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        basisFunctions = getBezierSurfaceBasisFunctions(degree)
+        for func in basisFunctions:
+            c = ax.plot_surface(T, S, eval(func,{'t':T,'s':S}),label=func)
+            c._facecolors2d=c._facecolors3d
+            c._edgecolors2d=c._edgecolors3d
+        ax.legend()
+        plt.title("Bezier surfaces for degree " + str(degree))
+        plt.xlabel('t')
+        plt.ylabel('s')
+        plt.show()
+        
+plotBezierBasisFunctions(type='curve',degree=3)
+
+#plotBezierBasisFunctions(type='surface',degree=1)
+
+        
+    
+    
