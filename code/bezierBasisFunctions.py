e2fb9694d23b487c4efe941d602080088fa1eea9
Aiman Khan <aiman97khan@gmail.com>
2019-10-12 20:49:31 +0500

    added documentation of functions

--- code/bezierBasisFunctions.py
+++ code/bezierBasisFunctions.py
@@ -7,11 +7,31 @@ from sympy import *
 from bspline import Bspline
 
 def getBezierCurveBasisFunctions(n):
+    """
+    Paramters:
+        n : degree of curve
+            n = 1 for linear, n = 2 for quadratic and so on
+    Returns:
+        List of basis functions for bezier curve for the given n
+    Example:
+        >>> getBezierCurveBasisFunctions(1)
+            ['-t + 1', 't']
+    """
     t = symbols('t')
     return [str(int(sp.comb(n,i))*((1-t)**(n-i))*(t**i)) for i in range(n+1) ]
 
 
 def getBezierSurfaceBasisFunctions(n):
+    """
+    Paramters:
+        n : degree of surface
+            n = 1 for linear, n = 2 for quadratic and so on
+    Returns:
+        List of basis functions for bezier surface for the given n
+    Example:
+        >>> getBezierSurfaceBasisFunctions(1)
+            ['(-s + 1)*(-t + 1)', 't*(-s + 1)', 's*(-t + 1)', 's*t']
+    """
     s = symbols('s')
     t = symbols('t')
     return [str(int(sp.comb(n,i))*((1-s)**(n-i))*(s**i)*int(sp.comb(n,j))*((1-t)**(n-j))*(t**j)) for i in range(n+1) for j in range(n+1) ]
@@ -23,6 +43,15 @@ def plotBsplineBasisFunctions(knot_vector,degree):
 '''
 
 def N_ik(knot,i,k,t):
+    """
+    Paramters:
+        knot : the knot vector of discrete values
+        i : this refers to the index of basis functions --> N_i,k
+        k : order of basis functions; degree = k - 1
+        t : interpolated vector of knot vector 
+    Returns:
+        N_i,k for a given knot vector
+    """
     if (k == 1):
         return np.array(list(map(int,(knot[i] <= t) & (knot[i+1] > t))))
     else:
@@ -30,12 +59,27 @@ def N_ik(knot,i,k,t):
         term2 = (knot[i+k] - t)/(knot[i+k]-knot[i+1])*N_ik(knot,i+1,k-1,t) if (knot[i+k]-knot[i+1]) != 0 else np.zeros(shape=(t.shape))
         return term1 + term2  
 
-def bSpline(knot, k,t):
+def bSplineBasis(knot, k,t):
+    """
+    Paramters:
+        knot : the knot vector of discrete values
+        k : order of basis functions; degree = k - 1
+        t : interpolated vector of knot vector 
+    Returns:
+        A list of all N_i,k for a given knot vector interpolated over t
+    """
     return [N_ik(knot,i,k,t) for i in range(len(knot)-k)]
             
 def plotBsplineBasisFunctions(knot, k):
+    """
+    Paramters:
+        knot : the knot vector of discrete values
+        k : order of basis functions; degree = k - 1 
+    Output:
+        Plots the B-spline basis functions
+    """
     t = np.linspace(knot[0],knot[-1],1000)
-    funcs = bSpline(knot, k,t)
+    funcs = bSplineBasis(knot, k,t)
     i = 0
     for func in  funcs:
         plt.plot(t,func,label = "N_" + str(i) + "," + str(k))
@@ -48,6 +92,14 @@ def plotBsplineBasisFunctions(knot, k):
     plt.show()
     
 def plotBezierBasisFunctions(type='curve',degree=1):
+    """
+    Paramters:
+        type : curve or surface
+        degree : degree of bezier basis functions, degree = 1 for linear and so on
+    Output:
+        Plots the bezier basis functions for curves if type is curve
+        Plots the bezier basis functions for surfaces if type is surface
+    """
     t = np.linspace(0,1,1000)
     if (type == 'curve'):
         basisFunctions = getBezierCurveBasisFunctions(degree)
@@ -78,6 +130,13 @@ def plotBezierBasisFunctions(type='curve',degree=1):
         
 
 def plotBezierCurves(controlPoints,degree):
+    """
+    Paramters:
+        controlPoints : A 2D list of control points (example : [[x1,y1],[x2,y2], ... , [xn,yn]]
+        degree : degree of bezier basis functions, degree = 1 for linear and so on
+    Output:
+        Plots the bezier curve controlled by a control polygon and also draws the control points
+    """
     if (len(controlPoints) != degree +1):
         raise ValueError('The number of control points should be 1 more than the degree')
     controlPoints = np.array(controlPoints)
@@ -90,7 +149,7 @@ def plotBezierCurves(controlPoints,degree):
         i += 1
     curveX , curveY = curve[:,0] , curve[:,1]
     plt.plot(curveX,curveY, label = "Bezier Curve")
-    plt.plot(cpX,cpY,'-og' , label = "Control Polygon")
+    plt.plot(cpX,cpY,'-og' , label = "Control Points")
     for i in range(len(cpX)):
         plt.annotate("P"+str(i), (cpX[i],cpY[i]), textcoords="offset points", xytext=(0,10), ha='center') 
     plt.title("Bezier curves for degree " + str(degree))
@@ -102,8 +161,9 @@ def plotBezierCurves(controlPoints,degree):
     plt.legend()
     plt.show()
 
+
 knot_vector = [0,0,0,1,1,1,2,2,3,3,4,5,5]
-plotBsplineBasisFunctions(knot_vector,4)         
+#plotBsplineBasisFunctions(knot_vector,4)         
 #plotBezierBasisFunctions(type='curve',degree=1)
 #plotBezierBasisFunctions(type='surface',degree=1)
 d = {2: [[0,0],[1,2],[2,0]], 1:[[0,0],[0.5,1]],3:[[0,0],[0.5,1],[0.5,0],[1,1]]}   
